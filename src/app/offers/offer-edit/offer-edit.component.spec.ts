e2e1e6ab959efc26a7f091751e58b1b650da6a6b
j.g.escriche@gmail.com <j.g.escriche@gmail.com>
2019-12-05 12:27:34 +0100

    sust firtname

--- src/app/offers/offer-edit/offer-edit.component.spec.ts
+++ src/app/offers/offer-edit/offer-edit.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { OfferEditComponent } from './offer-edit.component';
+import {FormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {OfferService} from '../offer.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRouteStub, RouterStub} from '../../testing/router-stubs';
+import {Offer} from '../offer';
+import {Observable, of} from 'rxjs';
+
+class OfferServiceStub {
+  getOfferById(): Observable<Offer> {
+    return of( { id: 1, title: 'James' } as Offer );
+  }
+}
 
 describe('OfferEditComponent', () => {
   let component: OfferEditComponent;
@@ -8,10 +22,25 @@ describe('OfferEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      declarations: [OfferEditComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      // schemas: [ NO_ERRORS_SCHEMA ],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [
+        {provide: OfferService, useClass: OfferServiceStub},
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
+      ]
+    })
+      .compileComponents();
+  }));
+
+ /* beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ OfferEditComponent ]
     })
     .compileComponents();
-  }));
+  }));*/
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OfferEditComponent);
@@ -19,7 +48,7 @@ describe('OfferEditComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create OwnerEditComponent', () => {
     expect(component).toBeTruthy();
   });
 });
