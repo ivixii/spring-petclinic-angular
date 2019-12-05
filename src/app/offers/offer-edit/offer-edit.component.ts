28602015eb5786a312d7231e2bd69fd51dadeb61
j.g.escriche@gmail.com <j.g.escriche@gmail.com>
2019-12-05 11:26:34 +0100

    modificacion offer

--- src/app/offers/offer-edit/offer-edit.component.ts
+++ src/app/offers/offer-edit/offer-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {OfferService} from '../offer.service';
+import {Offer} from '../offer';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-offer-edit',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferEditComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  offer: Offer;
+  errorMessage: string; // server error message
+  constructor(private offerService: OfferService, private route: ActivatedRoute, private router: Router) { 
+    this.offer = {} as Offer;
   }
 
+  ngOnInit() {
+
+    const ownerId = this.route.snapshot.params.id;
+    this.offerService.getOfferById(offerId).subscribe(
+      offer => this.offer = offer,
+      error => this.errorMessage = error as any);
+  }
+  onSubmit(offer: Offer) {
+    const that = this;
+    this.offerService.updateOffer(offer.id.toString(), offer).subscribe(
+      res => this.gotoOfferDetail(offer),
+      error => this.errorMessage = error as any
+    );
+  }
+  gotoOfferDetail(offer: Offer) {
+    this.errorMessage = null;
+    this.router.navigate(['/offers', offer.id]);
+  }
 }
