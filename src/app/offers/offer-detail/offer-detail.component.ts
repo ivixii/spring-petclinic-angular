cc2968432d813cfbe4e215eb1512e85a1cf22e1f
Carlos <acalonso95@gmail.com>
2019-12-05 11:06:00 +0100

    offer-detail

--- src/app/offers/offer-detail/offer-detail.component.ts
+++ src/app/offers/offer-detail/offer-detail.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+/*
+ *
+ *  * Copyright 2016-2017 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+/**
+ * @author Vitaliy Fedoriv
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {offerService} from '../offer.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {offer} from '../offer';
+
 
 @Component({
   selector: 'app-offer-detail',
   templateUrl: './offer-detail.component.html',
   styleUrls: ['./offer-detail.component.css']
 })
-export class OfferDetailComponent implements OnInit {
+export class offerDetailComponent implements OnInit {
+  errorMessage: string;
+  offer: offer;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private offerService: offerService) {
+    this.offer = {} as offer;
+  }
 
   ngOnInit() {
+    const offerId = this.route.snapshot.params.id;
+    this.offerService.getofferById(offerId).subscribe(
+      offer => this.offer = offer,
+      error => this.errorMessage = error as any);
   }
+
+  gotooffersList() {
+    this.router.navigate(['/offers']);
+  }
+
+  editoffer() {
+    this.router.navigate(['/offers', this.offer.id, 'edit']);
+  }
+
+  addPet(offer: offer) {
+    this.router.navigate(['/offers', offer.id, 'pets', 'add']);
+  }
+
 
 }
--- src/app/offers/offer.service.ts
+++ src/app/offers/offer.service.ts
@@ -1,9 +1,79 @@
-import { Injectable } from '@angular/core';
+/*
+ *
+ *  * Copyright 2016-2017 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
 
-@Injectable({
-  providedIn: 'root'
-})
+/**
+ * @author Vitaliy Fedoriv
+ */
+
+import {Injectable} from '@angular/core';
+import {Offer} from './offer';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {HandleError, HttpErrorHandler} from '../error.service';
+
+
+@Injectable()
 export class OfferService {
 
-  constructor() { }
+  entityUrl = environment.REST_API_URL + 'offers';
+
+  private readonly handlerError: HandleError;
+
+  constructor(private http: HttpClient, private httpErrorHandler: HttpErrorHandler) {
+    this.handlerError = httpErrorHandler.createHandleError('OfferService');
+  }
+
+  getOffers(): Observable<Offer[]> {
+    return this.http.get<Offer[]>(this.entityUrl)
+      .pipe(
+        catchError(this.handlerError('getOffers', []))
+      );
+  }
+
+  getOfferById(offerId: string): Observable<Offer> {
+    return this.http.get<Offer>(this.entityUrl + '/' + offerId)
+      .pipe(
+          catchError(this.handlerError('getOfferById', {} as Offer))
+      );
+  }
+
+  addOffer(offer: Offer): Observable<Offer> {
+    return this.http.post<Offer>(this.entityUrl, offer)
+      .pipe(
+        catchError(this.handlerError('addOffer', offer))
+      );
+  }
+
+  updateOffer(offerId: string, offer: Offer): Observable<{}> {
+    return this.http.put<Offer>(this.entityUrl + '/' + offerId, offer)
+      .pipe(
+        catchError(this.handlerError('updateOffer', offer))
+      );
+  }
+
+  deleteOffer(offerId: string): Observable<{}> {
+    return this.http.delete<Offer>(this.entityUrl + '/' + offerId)
+      .pipe(
+         catchError(this.handlerError('deleteOffer', [offerId]))
+      );
+  }
+
+
 }
